bfcdeed93dbf449a2dc51ef6b3820ac94c3b8e8a
Nehuen Lian Bova <nehuenn.lian@gmail.com>
2025-05-11 02:02:50 -0300

    sadfoiungoadfingmoiadfgadfg

--- src/views/check_stock_views.py
+++ src/views/check_stock_views.py
@@ -6,27 +6,79 @@ class CheckStockViewManager:
     def __init__(self, page: ft.Page, cont: ft.Column, stock_controller):
         self.page, self.cont = page, cont
         self.stock_controller = stock_controller
+        self.notification = Notifications(page)
+        self.components = UIComponents(self)
 
-        """
-        Inicializamos los componentes de la pagina en vacio 
-        de esta forma, seran "globales" para toda la clase
-        pudiendo agregarlos a cualquier metodo cuando quiera actualizar la vista
-        """
+        self.data_table = ft.Ref[ft.DataTable]()
 
-        self.title = None
-        self.divider = None
-        self.barcode = None
-        self.barcode_textfield = None
-        self.button = None
-        self.data_table = None
-        self.error_message = None
+        self.barcode_textfield = ft.Ref[ft.TextField]()
 
-        self.data_table_row = None
+        self._build_layout()
 
-        self.input_and_button_row = None
+    def _build_layout(self):
+        header = self.components.show_header()
+        search = self.components.show_search_controls()
+        table = self.components.show_info_table()
 
-        self.request_barcode()
+        self.cont.controls = [
+                            header,
+                            search,
+                            table,
+                        ]
 
+    def add_product_to_table(self, id, product_name):
+        new_row = ft.DataRow(
+            cells=[
+                ft.DataCell(ft.Text(id)), 
+                ft.DataCell(ft.Text(product_name)),
+                ])
+
+        self.data_table.current.rows = [new_row]
+        self.page.update()
+
+    # Handler
+    def search_product_handler(self, e):
+        barcode = self.barcode_textfield.current.value
+        if re.fullmatch("[A-Za-z0-9]+", barcode):
+            try:
+                product_name, available_quantity = self.stock_controller.check_product(barcode)
+                self.add_product_to_table(product_name, available_quantity)
+            except ProductNotFoundError:
+                self.notification.snack_bar_error_message("Producto no encontrado.")
+        else:
+            self.notification.snack_bar_error_message("No se permiten caracteres especiales.")
+
+class UIComponents:
+    def __init__(self, stock_views_manager):
+        self.stock_views_manager = stock_views_manager
+
+    def show_header(self):
+        title = ft.Row([ft.Text("Consultar Stock", size=30, weight=ft.FontWeight.BOLD)], alignment=ft.MainAxisAlignment.CENTER)
+        top_divider = ft.Divider(height=20, thickness=1)
+
+        return ft.Column([title, top_divider])
+    
+    def show_search_controls(self):
+        barcode_field =  ft.TextField(ref=self.stock_views_manager.barcode_textfield, label="Ingrese el código del producto...", width=300 ,on_submit=self.stock_views_manager.search_product_handler)
+        button = ft.ElevatedButton("Obtener", on_click=self.stock_views_manager.search_product_handler)
+
+        return ft.Row([barcode_field, button], spacing=20)
+    
+    def show_info_table(self):
+        table = ft.DataTable(ref=self.stock_views_manager.data_table,
+
+            columns = [
+                ft.DataColumn(ft.Text(value="Nombre del Producto")),
+                ft.DataColumn(ft.Text(value="Cantidad disponible")),
+            ])
+        
+        return ft.Container(content=ft.Column([table], scroll="auto"), height=400, alignment=ft.alignment.top_center)
+    
+class Notifications:
+    def __init__(self, page):
+        self.page = page
+ 
+    #  Notifications: display popup messages for error or success feedback in the UI
     def snack_bar_error_message(self, message):
         return self.page.open(ft.SnackBar(ft.Text(value=message, 
                                                 color=ft.Colors.WHITE, 
@@ -34,52 +86,3 @@ class CheckStockViewManager:
                                                 text_align=ft.TextAlign.CENTER,
                                                 size=20
                                                 ), bgcolor=ft.Colors.RED))
-
-    def request_barcode(self):
-        self.title = ft.Row([ft.Text("Consultar Stock", 
-                                     size=30, 
-                                    weight=ft.FontWeight.BOLD, 
-                                    color=ft.Colors.WHITE)], alignment=ft.MainAxisAlignment.CENTER)
-        
-        self.divider = ft.Divider(height=20, thickness=1, color=ft.Colors.GREY_600)
-        self.barcode_textfield = ft.TextField(label="Ingrese el código del producto...", on_submit=self.on_click_handler)
-        self.button = ft.ElevatedButton("Consultar", on_click=self.on_click_handler, bgcolor=ft.Colors.BLUE_400, color=ft.Colors.WHITE)
-        self.input_and_button_row = ft.Row([self.barcode_textfield, self.button], alignment=ft.MainAxisAlignment.CENTER)
-
-        self.cont.controls = [self.title, self.divider, self.input_and_button_row]
-
-    def on_click_handler(self, e):
-        barcode = self.barcode_textfield.value
-        if re.fullmatch("[A-Za-z0-9]+", barcode):
-            try:
-                product_name, available_quantity = self.stock_controller.check_product(barcode)
-                self.display_product(product_name, available_quantity)
-            except ProductNotFoundError:
-                self.snack_bar_error_message("Producto no encontrado.")
-        else:
-            self.snack_bar_error_message("No se permiten caracteres especiales.")
-
-    def display_product(self, product_name, available_quantity):
-
-        self.data_table = ft.DataTable(
-            bgcolor=ft.Colors.BLUE_GREY_900,
-            border=ft.border.all(2, ft.Colors.BLUE_GREY_500),
-            border_radius=10,
-            vertical_lines=ft.border.BorderSide(3, ft.Colors.BLUE_GREY_400),
-            horizontal_lines=ft.border.BorderSide(1, ft.Colors.BLUE_GREY_400),
-            columns = [
-                ft.DataColumn(ft.Text(value="Nombre del Producto", color=ft.Colors.WHITE)),
-                ft.DataColumn(ft.Text(value="Cantidad disponible", color=ft.Colors.WHITE)),
-            ])
-
-        new_row = ft.DataRow(
-            cells=[
-                ft.DataCell(ft.Text(product_name)), 
-                ft.DataCell(ft.Text(available_quantity))
-                ])
-        
-        self.data_table_row = ft.Row([self.data_table], ft.MainAxisAlignment.CENTER)
-
-        self.data_table.rows.append(new_row)
-        self.cont.controls = [self.title, self.divider, self.input_and_button_row, self.data_table_row]
-        self.page.update()
--- src/views/price_management_views.py
+++ src/views/price_management_views.py
@@ -6,34 +6,150 @@ class PriceViewsManager:
     def __init__(self, page: ft.Page, cont: ft.Column, prices_controller):
         self.page, self.cont = page, cont
         self.prices_controller = prices_controller
+        self.notification = Notifications(page)
+        self.components = UIComponents(self)
 
-        self.title = None
-        self.divider = None
-        self.barcode_textfield = None
-        self.search_button = None
-        self.data_table = None
-        self.data_table_row = None
-        self.update_price_row = None
-        self.confirm_question_row = None
+        self.data_table = ft.Ref[ft.DataTable]()
+        self.barcode_textfield = ft.Ref[ft.TextField]()
 
-        self.current_price_label = None
-        self.new_price = None
-
-        self.confirm_label = None
-        self.no_button = None
-        self.yes_button = None
+        self.current_price_value = None
+        self.current_price_label = ft.Ref[ft.Text]()
+        self.new_price_textfield = ft.Ref[ft.TextField]()
 
         self.product_id = None
-        self.new_price_textfield = None
-        self.change_price_button = None
+        self.final_alert_dialog = None
 
-        self.error_message = None
-        self.success_message = None
+        self._build_first_layout()
 
-        self.input_and_button_row = None
+    def _build_first_layout(self):
+        header = self.components.show_header()
+        search = self.components.show_search_controls()
+        table = self.components.show_info_table()
 
-        self.request_barcode()
+        self.cont.controls = [
+                            header,
+                            search,
+                            table,
+                        ]
 
+        self.page.update()
+
+    def _build_second_layout(self):
+        update_price_form = self.components.show_price_update_form()
+        self.cont.controls.extend([update_price_form])
+        
+        self.page.update()
+
+    def add_product_to_table(self, id, product_name, current_price):
+        new_row = ft.DataRow(
+            cells=[
+                ft.DataCell(ft.Text(id)), 
+                ft.DataCell(ft.Text(product_name)),
+                ft.DataCell(ft.Text(f"${current_price}"))
+                ])
+        
+        self.current_price_value = current_price
+
+        self.data_table.current.rows = [new_row]
+        self.page.update()
+
+    # Handlers
+    def search_product_handler(self, e):
+        barcode = self.barcode_textfield.current.value
+        if re.fullmatch("[A-Za-z0-9]+", barcode):
+            try:
+                id, product_name, actual_price = self.prices_controller.check_product(barcode)
+                self.product_id = id
+                self.add_product_to_table(id, product_name, actual_price)
+                self._build_second_layout()
+            except ProductNotFoundError:
+                self.notification.snack_bar_error_message("Producto no encontrado.")
+        else:
+            self.notification.snack_bar_error_message("No se permiten caracteres especiales.")
+
+    def alert_dialog_handler(self):
+        self.final_alert_dialog = self.components.confirm_changes_modal()
+        self.page.open(self.final_alert_dialog)
+
+    def confirm_changes_handler(self, e):
+        self.prices_controller.update_price(self.product_id, self.new_price_textfield.current.value)
+        self.notification.snack_bar_success_message("Precio actualizado correctamente.")
+        self.page.close(self.final_alert_dialog)
+        self.reset_page_default()
+
+    def update_price_handler(self, e):
+        try:
+            self.new_price = float(self.new_price_textfield.current.value)
+        except ValueError:
+            self.notification.snack_bar_error_message("Por favor, ingrese un valor.")
+
+        else:
+            if self.new_price > 0:
+                self.alert_dialog_handler()
+            elif self.new_price <= 0:
+                self.notification.snack_bar_error_message("No se puede ingresar un precio menor a $0.")
+
+    # Aux
+    def cancel_operation(self, e):
+        self.notification.snack_bar_success_message("Operación cancelada.")
+        self.page.close(self.final_alert_dialog)
+        self.reset_page_default()
+
+    def reset_page_default(self):
+        self._build_first_layout()
+
+class UIComponents:
+    def __init__(self, prices_views_manager):
+        self.prices_views_manager = prices_views_manager
+
+    def show_header(self):
+        title = ft.Row([ft.Text("Gestionar Precios", size=30, weight=ft.FontWeight.BOLD)], alignment=ft.MainAxisAlignment.CENTER)
+        top_divider = ft.Divider(height=20, thickness=1)
+
+        return ft.Column([title, top_divider])
+    
+    def show_search_controls(self):
+        barcode_field =  ft.TextField(ref=self.prices_views_manager.barcode_textfield, label="Ingrese el código del producto...", width=300 ,on_submit=self.prices_views_manager.search_product_handler)
+        button = ft.ElevatedButton("Obtener", on_click=self.prices_views_manager.search_product_handler)
+
+        return ft.Row([barcode_field, button], spacing=20)
+    
+    def show_info_table(self):
+        table = ft.DataTable(ref=self.prices_views_manager.data_table,
+
+            columns = [
+                ft.DataColumn(ft.Text(value="ID")),
+                ft.DataColumn(ft.Text(value="Nombre del Producto")),
+                ft.DataColumn(ft.Text(value="Precio actual")),
+            ])
+        
+        return ft.Container(content=ft.Column([table], scroll="auto"), height=400, alignment=ft.alignment.top_center)
+    
+    def show_price_update_form(self):
+        current_price = ft.Text(ref=self.prices_views_manager.current_price_label, value=f"Precio actual: ${self.prices_views_manager.current_price_value}")
+        new_price_label = ft.Text(value=f"Nuevo precio:")
+        new_price_textfield = ft.TextField(ref=self.prices_views_manager.new_price_textfield, label="$ ", 
+                                        input_filter=ft.InputFilter(allow=True, regex_string=r"^\d*(\.\d*)?$"))
+        
+        update_price_button = ft.ElevatedButton("Actualizar", on_click=self.prices_views_manager.update_price_handler)
+
+        return ft.Row([current_price, new_price_label, new_price_textfield, update_price_button])
+ 
+    def confirm_changes_modal(self):
+        return ft.AlertDialog(
+        modal=True,
+        content=ft.Text("¿Confirmar cambios?"),
+        actions=[
+            ft.TextButton("Confirmar", on_click=self.prices_views_manager.confirm_changes_handler),
+            ft.TextButton("Cancelar", on_click=self.prices_views_manager.cancel_operation),
+        ],
+        actions_alignment=ft.MainAxisAlignment.END)
+
+class Notifications:
+    def __init__(self, page):
+        self.page = page
+ 
+    #  Notifications: display popup messages for error or success feedback in the UI
     def snack_bar_error_message(self, message):
         return self.page.open(ft.SnackBar(ft.Text(value=message, 
                                                 color=ft.Colors.WHITE, 
@@ -49,111 +165,3 @@ class PriceViewsManager:
                                                 text_align=ft.TextAlign.CENTER,
                                                 size=20
                                                 ), bgcolor=ft.Colors.GREEN))
-
-    def request_barcode(self):
-        self.title = ft.Row([ft.Text("Gestionar Precios", 
-                                    size=30, 
-                                    weight=ft.FontWeight.BOLD, 
-                                    color=ft.Colors.WHITE)], alignment=ft.MainAxisAlignment.CENTER)
-        
-        self.divider = ft.Divider(height=20, thickness=1, color=ft.Colors.GREY_600)
-        self.barcode_textfield = ft.TextField(label="Ingrese el código del producto...", width=300 ,on_submit=self.search_product_handler)
-        self.search_button = ft.ElevatedButton("Obtener", 
-                                            on_click=self.search_product_handler,
-                                            bgcolor=ft.Colors.BLUE_400, color=ft.Colors.WHITE)
-        self.input_and_button_row = ft.Row([self.barcode_textfield, self.search_button], alignment=ft.MainAxisAlignment.CENTER)
-
-        self.cont.controls = [self.title, self.divider, self.input_and_button_row]
-        self.page.update()
-
-    def display_product(self, id, product_name, current_price):
-
-        self.data_table = ft.DataTable(
-            bgcolor=ft.Colors.BLUE_GREY_900,
-            border=ft.border.all(2, ft.Colors.BLUE_GREY_500),
-            border_radius=10,
-            vertical_lines=ft.border.BorderSide(3, ft.Colors.BLUE_GREY_400),
-            horizontal_lines=ft.border.BorderSide(1, ft.Colors.BLUE_GREY_400),
-            columns = [
-                ft.DataColumn(ft.Text(value="ID", color=ft.Colors.WHITE)),
-                ft.DataColumn(ft.Text(value="Nombre del Producto", color=ft.Colors.WHITE)),
-                ft.DataColumn(ft.Text(value="Precio actual", color=ft.Colors.WHITE)),
-            ])
-
-        new_row = ft.DataRow(
-            cells=[
-                ft.DataCell(ft.Text(id)), 
-                ft.DataCell(ft.Text(product_name)),
-                ft.DataCell(ft.Text(f"${current_price}"))
-                ])
-        
-        self.data_table_row = ft.Row([self.data_table], ft.MainAxisAlignment.CENTER)
-
-        self.data_table.rows.append(new_row)
-        self.cont.controls = [self.title, self.divider, self.input_and_button_row, self.data_table_row]
-        self.page.update()
-
-    def set_new_price(self, current_price) -> float:
-        self.current_price_label = ft.Text(value=f"Precio actual: ${current_price}")
-        self.new_price_textfield = ft.TextField(label="$ ", input_filter=ft.InputFilter(allow=True, regex_string=r"^\d*(\.\d*)?$"))
-        self.change_price_button = ft.ElevatedButton("Actualizar", bgcolor=ft.Colors.BLUE_400, color=ft.Colors.WHITE, on_click=self.confirm_changes_qt)
-
-        self.update_price_row = ft.Row([self.current_price_label, self.new_price_textfield, self.change_price_button], alignment=ft.MainAxisAlignment.CENTER)
-
-        self.cont.controls = [self.title,
-                            self.divider,
-                            self.input_and_button_row, 
-                            self.data_table_row, 
-                            self.update_price_row]
-        
-        self.page.update()
-
-    def confirm_changes_qt(self, e):
-        self.confirm_label = ft.Text("Confirmar cambios", weight=ft.FontWeight.BOLD)
-        self.no_button = ft.ElevatedButton("Aceptar",
-                                           on_click=self.update_price_handler,
-                                            bgcolor=ft.Colors.BLUE_400, color=ft.Colors.WHITE)
-        
-        self.yes_button = ft.ElevatedButton("Cancelar", bgcolor=ft.Colors.BLUE_400, color=ft.Colors.WHITE, on_click=self.cancel_operation)
-
-        self.confirm_question_row = ft.Row([self.confirm_label, self.no_button, self.yes_button], alignment=ft.MainAxisAlignment.CENTER)
-
-        self.cont.controls = [self.title,
-                            self.divider,
-                            self.input_and_button_row, 
-                            self.data_table_row, 
-                            self.update_price_row,
-                            self.confirm_question_row]
-
-        self.page.update()
-
-    # Handlers
-    def search_product_handler(self, e):
-        barcode = self.barcode_textfield.value
-        if re.fullmatch("[A-Za-z0-9]+", barcode):
-            try:
-                id, product_name, current_price = self.prices_controller.check_product(barcode)
-                self.product_id = id
-                self.display_product(id, product_name, current_price)
-                self.set_new_price(current_price)
-            except ProductNotFoundError:
-                self.snack_bar_error_message("Producto no encontrado.")
-        else:
-            self.snack_bar_error_message("No se permiten caracteres especiales.")
-
-    def update_price_handler(self, e):
-        self.new_price = float(self.new_price_textfield.value)
-        if self.new_price > 0:
-            self.prices_controller.update_price(self.product_id, self.new_price_textfield.value)
-            self.snack_bar_success_message("Precio actualizado correctamente.")
-            self.reset_page_default()
-        elif self.new_price <= 0:
-            self.snack_bar_error_message("No se puede ingresar un precio menor a $0.")
-
-    # Aux
-    def cancel_operation(self, e):
-        self.snack_bar_success_message("Operación cancelada correctamente.")
-        self.request_barcode()
-
-    def reset_page_default(self):
-        self.request_barcode()
