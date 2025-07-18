17a51a691b7651677143f9f75445d2655e11d569
Nehuen Lian Bova <nehuenn.lian@gmail.com>
2025-07-18 01:16:53 -0300

    simplified manage_prices controller

--- src/controllers/manage_prices.py
+++ src/controllers/manage_prices.py
@@ -13,37 +13,24 @@ class PricesManagementController:
         return self._view
     
     @view.setter
-    def view(self, view):
+    def view(self, view) -> None:
         self._view = view
 
-    def remove_soon(self, barcode):
-        print(f"Obteniendo producto... {barcode}")
+    def get_product(self, barcode: str) -> None:
+        try:
+            product_id, product_barcode, product_name, available_quantity = self.change_prices.search_product(barcode)
+            self._view.display_product(product_id, product_barcode, product_name, available_quantity)
 
-    def get_barcode(self) -> list:
-        self.view.show_message("El usuario eligió gestionar precios.")
+        except ProductNotFoundError as e:
+            print("Producto no encontrado.")
+        except Exception as e:
+            print(f"Error inesperado: {e}")
 
-        getting_product = True
-        while getting_product:
-            try:
-                    barcode = self.view.request_barcode()
-                    if barcode:
-                        [id, product_name, actual_price] = self.change_prices.search_product(barcode)
-                        product_properties = [id, product_name, actual_price]
-                        getting_product = False
-                        controller_logger.info(f'Get barcode process successfully ended. User input barcode "{barcode}" Actual price: {actual_price}.')
-                        return product_properties
-            except ProductNotFoundError:
-                    self.view.show_message("Producto no encontrado.")
-            except Exception as e:
-                    self.view.show_message(f"Error desconocido: {e}")
-
-    def show_product_info(self, product_properties):
-        self.view.display_product(product_properties[1], product_properties[2])
-
-    def choose_new_price(self, product_properties) -> float:
+    def choose_new_price(self, product_properties: list) -> float:
         new_price = self.view.set_new_price()
         self.view.display_prices(product_properties[2], new_price)
         controller_logger.info(f'New price: ${new_price}')
+
         return new_price
     
     def confirming_process(self) -> str:
@@ -54,10 +41,8 @@ class PricesManagementController:
         try:
             self.change_prices.update_prices(product_properties[0], new_price)
             self.view.show_message("El cambio de precio se realizó correctamente.")
+
         except TransactionIntegrityError as e:
             self.view.show_message(f"Ocurrió un error al actualizar el precio en el registro, inténtalo de nuevo.")
         except Exception as e:
             self.view.show_message(f"Error desconocido: {e}")
-
-    def manage_update_prices_process(self):
-        print("Hola, estas en manage_prices")
