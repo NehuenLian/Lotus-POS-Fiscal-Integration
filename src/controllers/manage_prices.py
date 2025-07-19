7b8b2bb00e6bfa8603d8ec8855261beb2f7f8ca7
Nehuen Lian Bova <nehuenn.lian@gmail.com>
2025-07-19 17:19:16 -0300

    finish manage_prices section

--- src/controllers/manage_prices.py
+++ src/controllers/manage_prices.py
@@ -16,33 +16,23 @@ class PricesManagementController:
     def view(self, view) -> None:
         self._view = view
 
-    def get_product(self, barcode: str) -> None:
+    def get_product(self, barcode: str) -> int:
         try:
             product_id, product_barcode, product_name, available_quantity = self.change_prices.search_product(barcode)
             self._view.display_product(product_id, product_barcode, product_name, available_quantity)
+            return product_id
 
         except ProductNotFoundError as e:
             print("Producto no encontrado.")
         except Exception as e:
             print(f"Error inesperado: {e}")
-
-    def choose_new_price(self, product_properties: list) -> float:
-        new_price = self.view.set_new_price()
-        self.view.display_prices(product_properties[2], new_price)
-        controller_logger.info(f'New price: ${new_price}')
-
-        return new_price
     
-    def confirming_process(self) -> str:
-        confirm_choice = self.view.confirm_changes_qt()
-        return confirm_choice
-    
-    def update_price(self, product_properties, new_price):
+    def update_price(self, product_id: int, new_price: float):
         try:
-            self.change_prices.update_prices(product_properties[0], new_price)
-            self.view.show_message("El cambio de precio se realizó correctamente.")
+            self.change_prices.update_prices(product_id, new_price)
+            print("El cambio de precio se realizó correctamente.")
 
         except TransactionIntegrityError as e:
-            self.view.show_message(f"Ocurrió un error al actualizar el precio en el registro, inténtalo de nuevo.")
+            print(f"Ocurrió un error al actualizar el precio en el registro, inténtalo de nuevo.")
         except Exception as e:
-            self.view.show_message(f"Error desconocido: {e}")
+            print(f"Error desconocido: {e}")
--- src/views/manage_prices.py
+++ src/views/manage_prices.py
@@ -3,7 +3,7 @@ from typing import Optional
 from PySide6.QtCore import Qt
 from PySide6.QtWidgets import (QApplication, QFrame, QHBoxLayout, QHeaderView,
                                QLabel, QLineEdit, QPushButton, QStackedWidget,
-                               QTableWidget, QVBoxLayout, QWidget, QTableWidgetItem)
+                               QTableWidget, QVBoxLayout, QWidget, QTableWidgetItem, QMessageBox)
 
 from src.views.shared_components import (display_header, display_send_button,
                                          display_textfield)
@@ -12,10 +12,9 @@ from src.views.shared_components import (display_header, display_send_button,
 class PriceViewManager(QWidget):
     def __init__(self, manage_prices_controller):
         super().__init__()
-        self.resize(1280, 720)
 
         self.components = DomainComponents()
-        self.main_window = QVBoxLayout(self)
+        self.main_layout = QVBoxLayout(self)
         self.manage_prices_controller = manage_prices_controller
 
         self._set_main_layout()
@@ -24,12 +23,15 @@ class PriceViewManager(QWidget):
     def _set_main_layout(self) -> None:
 
         header_layout = self._set_header()
-        first_layout = self._set_first_layout()
-        second_layout = self._set_second_layout()
+        search_layout = self._set_search_layout()
+        table_layout = self._set_table_layout()
+        update_price_layout = self._set_update_price_section()
 
-        self.main_window.addWidget(header_layout)
-        self.main_window.addWidget(first_layout)
-        self.main_window.addWidget(second_layout)
+        self.main_layout.addWidget(header_layout)
+        self.main_layout.addWidget(search_layout)
+        self.main_layout.addWidget(table_layout)
+        self.main_layout.addWidget(update_price_layout)
+        self.main_layout.addStretch(1)
 
     def _set_header(self) -> QWidget:
         header_layout = QHBoxLayout()
@@ -42,45 +44,77 @@ class PriceViewManager(QWidget):
 
         return header_qwidget
 
-    def _set_first_layout(self) -> QWidget:
-        first_layout = QHBoxLayout()
-        first_qwidget = QWidget()
+    def _set_search_layout(self) -> QWidget:
+        search_layout = QHBoxLayout()
+        search_qwidget = QWidget()
 
         self.barcode_input_field = display_textfield(self._search_product_handler)
 
         send_button = display_send_button(self._search_product_handler)
 
-        first_layout.addWidget(self.barcode_input_field)
-        first_layout.addWidget(send_button)
-        first_layout.addStretch(1)
+        search_layout.addWidget(self.barcode_input_field)
+        search_layout.addWidget(send_button)
+        search_layout.addStretch(1)
 
-        first_qwidget.setLayout(first_layout)
+        search_qwidget.setLayout(search_layout)
 
-        return first_qwidget
+        return search_qwidget
     
-    def _set_second_layout(self) -> QWidget:
+    def _set_table_layout(self) -> QWidget:
 
-        second_layout = QVBoxLayout()
-        second_qwidget = QWidget()
+        table_layout = QVBoxLayout()
+        table_qwidget = QWidget()
 
         self.table = self._display_table()
+        divider = self._display_divider()
 
-        second_layout.addWidget(self.table, alignment=Qt.AlignHCenter)
-        second_layout.addStretch(1)
-        second_qwidget.setLayout(second_layout)
+        table_layout.addWidget(self.table, alignment=Qt.AlignHCenter)
+        table_layout.addSpacing(15)
+        table_layout.addWidget(divider)
+        table_layout.addStretch(1)
+        table_qwidget.setLayout(table_layout)
 
-        return second_qwidget
+        return table_qwidget
+    
+    def _set_update_price_section(self) -> QWidget:
+        
+        update_price_layout = QHBoxLayout()
+        update_price_qwidget = QWidget()
+
+        new_price_label, self.new_price_field, update_price_button = self._display_update_price_form()
+
+        update_price_layout.addStretch(1)
+        update_price_layout.addWidget(new_price_label)
+        update_price_layout.addWidget(self.new_price_field)
+        update_price_layout.addWidget(update_price_button)
+        update_price_layout.addStretch(1)
+        update_price_qwidget.setLayout(update_price_layout)
+
+        return update_price_qwidget
     
     # Individual components
     def _display_table(self) -> QTableWidget:
         table = self.components.info_table()
         return table
     
+    def _display_divider(self) -> QFrame:
+        divider = self.components.horizontal_divider()
+        return divider
+    
+    def _display_update_price_form(self) -> tuple[QLabel, QLineEdit, QPushButton]:
+        new_price_label = self.components.input_new_price_label()
+        new_price_field = self.components.new_price_field()
+        update_price_button = self.components.update_price()
+
+        update_price_button.clicked.connect(self._update_price_handler)
+
+        return new_price_label, new_price_field, update_price_button
+    
     # Actions handlers
     def _search_product_handler(self) -> None:
         barcode = self.barcode_input_field.text()
         self.barcode_input_field.clear()
-        self.manage_prices_controller.get_product(barcode)
+        self.product_id = self.manage_prices_controller.get_product(barcode)
 
     # Update and manipulate view
     def display_product(self, product_id: int, product_barcode: Optional[str], product_name: str, product_price: Optional[int]) -> None:
@@ -91,6 +125,22 @@ class PriceViewManager(QWidget):
         self.table.setItem(0, 2, QTableWidgetItem(product_name))
         self.table.setItem(0, 3, QTableWidgetItem(str(product_price) if product_price is not None else "0"))
 
+    def _update_price_handler(self) -> None:
+        new_price = self.new_price_field.text()
+        choice = self.components.confirm_process_message_box()
+
+        if choice:
+            self.manage_prices_controller.update_price(self.product_id, new_price)
+        else:
+            # TODO: Put here another messagebox notifying the price change
+            pass
+        
+        self._clear_view()
+
+    # Auxiliar
+    def _clear_view(self) -> None:
+        self.table.setRowCount(0)
+        self.new_price_field.clear()
 
 class DomainComponents:
     def __init__(self):
@@ -110,3 +160,45 @@ class DomainComponents:
         table.setVerticalScrollBarPolicy(Qt.ScrollBarAsNeeded)
 
         return table
+    
+    def horizontal_divider(self) -> QFrame:
+        line = QFrame()
+        line.setFrameShape(QFrame.HLine)
+        line.setFrameShadow(QFrame.Sunken)
+
+        return line
+    
+    def input_new_price_label(self) -> QLabel:
+        new_price_label = QLabel("Nuevo precio:  ")
+        return new_price_label
+    
+    def new_price_field(self) -> QLineEdit:
+        new_price_field = QLineEdit()
+        new_price_field.setPlaceholderText("$...")
+        new_price_field.setFixedWidth(200)
+        new_price_field.setFixedHeight(30)
+        new_price_field.setMaxLength(40)
+
+        return new_price_field
+    
+    def update_price(self) -> QPushButton:
+        update_button = QPushButton("Actualizar")
+        return update_button
+    
+    def confirm_process_message_box(self) -> bool:
+        msg_box = QMessageBox()
+        msg_box.setWindowTitle("Confirmar cambio de precio")
+        msg_box.setText("¿Confirmar?")
+        msg_box.setIcon(QMessageBox.Question)
+
+        confirm = msg_box.addButton("Confirmar", QMessageBox.AcceptRole)
+        cancel = msg_box.addButton("Cancelar", QMessageBox.RejectRole)
+
+        msg_box.exec()
+
+        if msg_box.clickedButton() == confirm:
+            return True
+        else:
+            return False
+        
+    # TODO: Make another messagebox that confirms the price update was aborted.
