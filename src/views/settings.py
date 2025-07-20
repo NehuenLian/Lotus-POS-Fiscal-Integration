b3cd03cd39b83fa124f4ad53fb1eb2a5172e7fc8
Nehuen Lian Bova <nehuenn.lian@gmail.com>
2025-07-20 19:13:44 -0300

    add stretch to settings header

--- src/views/settings.py
+++ src/views/settings.py
@@ -1,7 +1,6 @@
 from PySide6.QtCore import Qt
-from PySide6.QtWidgets import (QHBoxLayout, QHeaderView,
-                               QTableWidget, QTableWidgetItem,
-                               QVBoxLayout, QWidget)
+from PySide6.QtWidgets import (QHBoxLayout, QHeaderView, QTableWidget,
+                               QTableWidgetItem, QVBoxLayout, QWidget)
 
 from src.views.shared_components import (display_header, display_send_button,
                                          display_textfield,
@@ -12,7 +11,7 @@ class SettingsViewManager(QWidget):
     def __init__(self, settings_controller):
         super().__init__()
         self.components = DomainComponents()
-        self.main_window = QVBoxLayout(self)
+        self.main_layout = QVBoxLayout(self)
         
         self.settings_controller = settings_controller
 
@@ -21,18 +20,20 @@ class SettingsViewManager(QWidget):
     # Set layouts
     def _set_main_layout(self) -> None:
         header_layout = self._set_header()
-        self.main_window.addWidget(header_layout)
+
+        self.main_layout.addWidget(header_layout)
+        self.main_layout.addStretch(1)
 
     def _set_header(self) -> QWidget:
-            header_layout = QHBoxLayout()
-            header_qwidget = QWidget()
+        header_layout = QHBoxLayout()
+        header_qwidget = QWidget()
 
-            header = display_header("Ajustes")
+        header = display_header("Ajustes")
 
-            header_layout.addWidget(header)
-            header_qwidget.setLayout(header_layout)
+        header_layout.addWidget(header)
+        header_qwidget.setLayout(header_layout)
 
-            return header_qwidget
+        return header_qwidget
     
 
 class DomainComponents:
