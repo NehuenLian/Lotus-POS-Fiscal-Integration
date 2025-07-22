c814a1de5e8793d1904c55f28deb91daaa063270
Nehuen Lian Bova <nehuenn.lian@gmail.com>
2025-07-21 21:23:47 -0300

    remove __init__.py, now the connection is entirely handled by the context manager

--- src/data_access/session_manager.py
+++ src/data_access/session_manager.py
@@ -1,11 +1,18 @@
+import os
 from contextlib import contextmanager
+
+from dotenv import load_dotenv
 
 from src.data_access.connection import DataBaseConnection
 
 
 @contextmanager
-def session_scope(connection: DataBaseConnection):
+def session_scope():
+    load_dotenv()
+    db_url = os.getenv("DB_URL")
+    connection = DataBaseConnection(db_url)
     session = connection.get_session()
+
     try:
         yield session
         session.commit()
