9eb8f2994b13739e67d323ecad948023dbfaef9f
Nehuen Lian Bova <nehuenn.lian@gmail.com>
2025-07-23 13:52:25 -0300

    load env in program start. update os.environ in update_db method

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+from dotenv import load_dotenv
 from PySide6.QtWidgets import QApplication
 
 from src.controllers.main_controller import MainController
 
 if __name__ == "__main__":
     app = QApplication([])
+    load_dotenv(override=True)
     controller = MainController()
     controller.ui.show()
     app.exec()
--- src/business_logic/settings.py
+++ src/business_logic/settings.py
@@ -2,6 +2,7 @@ from pathlib import Path
 
 from dotenv import set_key
 
+import os
 from src.utils.logger_config import business_logger
 
 
@@ -12,4 +13,6 @@ class SettingsManagement:
     def update_db_url(self, db_url: str) -> None:
         env_path = Path(".env")  # or absolute path
         set_key(dotenv_path=env_path, key_to_set="DB_URL", value_to_set=db_url)
+        os.environ["DB_URL"] = db_url
+        
         business_logger.info(f"Database URL successfully updated to: {db_url}")
