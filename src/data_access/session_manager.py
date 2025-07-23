7054de4e61a9f54582b58c058d0d74754f6a0bf9
Nehuen Lian Bova <nehuenn.lian@gmail.com>
2025-07-23 13:53:13 -0300

    remove load_dotenv()

--- src/data_access/session_manager.py
+++ src/data_access/session_manager.py
@@ -1,14 +1,11 @@
 import os
 from contextlib import contextmanager
 
-from dotenv import load_dotenv
-
 from src.data_access.connection import DataBaseConnection
 
 
 @contextmanager
 def session_scope():
-    load_dotenv()
     db_url = os.getenv("DB_URL")
     connection = DataBaseConnection(db_url)
     session = connection.get_session()
