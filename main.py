e585cc3bf00809cdd02a158a951b92fe045d719b
Nehuen Lian <nehuenn.lian@gmail.com>
2025-07-31 16:27:42 -0300

    add styles

--- main.py
+++ main.py
@@ -3,8 +3,13 @@ from PySide6.QtWidgets import QApplication
 
 from src.controllers.main_controller import MainController
 
+def load_stylesheet(app):
+    with open("src/views/assets/styles.qss", "r") as f:
+        app.setStyleSheet(f.read())
+
 if __name__ == "__main__":
     app = QApplication([])
+    load_stylesheet(app)
     load_dotenv(override=True)
     controller = MainController()
     controller.ui.show()
